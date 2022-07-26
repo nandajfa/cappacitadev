f52916ead6a771a8b1e4c6dae8ea66651ef89201
Jessica Fernanda <80687429+nandajfa@users.noreply.github.com>
2022-07-26 19:03:43 -0300

    Add files via upload

---
+++ Capacita dev/name.sql
@@ -0,0 +1,9 @@
+# UPDATE 
+
+#UPDATE produtos SET quantidade_estoque = 40 WHERE id = 3
+
+#UPDATE produtos SET quantidade_estoque = 30, valor = 55.00 WHERE nome_produto = 'Mouse'
+
+#SELECT * FROM produtos
+#UPDATE produtos SET quantidade_estoque = 5 WHERE nome_produto LIKE 'Mesa%' 
+#UPDATE produtos SET quantidade_estoque = quantidade_estoque - 1 WHERE nome_produto  
